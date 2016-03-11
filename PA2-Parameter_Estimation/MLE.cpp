3310f6df6c0d1d46a3d2839c7cafff6afeff9178
Tim Kwist <Timkwist@hotmail.com>
2016-03-10 16:33:02 -0800

    Fix warnings on for loops

--- PA2-Parameter_Estimation/MLE.cpp
+++ PA2-Parameter_Estimation/MLE.cpp
@@ -3,7 +3,7 @@
 Vector2f MLE::calculateSampleMean(vector<Vector2f> data)
 {
 	Vector2f sum;
-	for(int i = 0; i < data.size(); i++)
+	for(vector<int>::size_type i = 0; i < data.size(); i++)
 	{
 		sum += data[i];
 	}
@@ -13,7 +13,7 @@ Vector2f MLE::calculateSampleMean(vector<Vector2f> data)
 Matrix2f MLE::calculateSampleCovariance(vector<Vector2f> data, Vector2f sampleMean)
 {
 	Matrix2f sum;
-	for(int i = 0; i <  data.size(); i++)
+	for(vector<int>::size_type i = 0; i <  data.size(); i++)
 	{
 		sum += (sampleMean - data[i])*(sampleMean -data[i]).transpose();
 	}
