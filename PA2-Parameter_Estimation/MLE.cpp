a9da94ec27d67910223d2cf140f2e83685d91762
shane-melton <smelton@nevada.unr.edu>
2016-03-10 17:19:32 -0800

    Part 1A working again

--- PA2-Parameter_Estimation/MLE.cpp
+++ PA2-Parameter_Estimation/MLE.cpp
@@ -3,6 +3,7 @@
 Vector2f MLE::calculateSampleMean(vector<Vector2f> data)
 {
 	Vector2f sum;
+	sum << 0.0, 0.0;
 	for(vector<int>::size_type i = 0; i < data.size(); i++)
 	{
 		sum += data[i];
@@ -13,9 +14,11 @@ Vector2f MLE::calculateSampleMean(vector<Vector2f> data)
 Matrix2f MLE::calculateSampleCovariance(vector<Vector2f> data, Vector2f sampleMean)
 {
 	Matrix2f sum;
+	sum << 	0.0, 0.0,
+			0.0, 0.0;
 	for(vector<int>::size_type i = 0; i <  data.size(); i++)
 	{
-		sum += (sampleMean - data[i])*(sampleMean -data[i]).transpose();
+		sum += (sampleMean - data[i])*((sampleMean -data[i]).transpose());
 	}
 	return sum / data.size();
 }
--- PA2-Parameter_Estimation/main.cpp
+++ PA2-Parameter_Estimation/main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include "BayesClassifier.h"
 #include "SampleGenerator.h"
+#include "MLE.h"
+#include <cstdlib>
 
 #include <iostream>
 #include <fstream>
@@ -11,6 +13,8 @@ using namespace Eigen;
 using namespace std;
 
 void writeSamplesToFile(const char* fileName, vector<Vector2f> sampleOne, vector<Vector2f> sampleTwo);
+
+int randIndex(int size);
 
 int main()
 {
@@ -23,13 +27,14 @@ int main()
 	Matrix2f sigmaOne, sigmaTwo;
 	Vector2f muOne, muTwo;
 
-	float priorOne, priorTwo;
-
 	BayesClassifier classifier;
 	SampleGenerator generator;
 
 	vector<Vector2f> sampleOne, sampleTwo;
 	vector<Vector2f> sampleMis;
+
+	Matrix2f estSigmaOne, estSigmaTwo;
+	Vector2f estMuOne, estMuTwo;
 	
 	int misclassifiedOne, misclassifiedTwo;
 
@@ -45,7 +50,6 @@ int main()
 	sigmaTwo << 2.0, 0.0,
 				0.0, 2.0;
 
-	priorOne = priorTwo = 0.5;
 	misclassifiedOne = misclassifiedTwo = 0;
 
 	sampleOne = generator.generateSamples(muOne, sigmaOne);
@@ -88,14 +92,11 @@ int main()
 	// Begin Part 1A Tests (Esimated Covaraince and Mean)
 	//================================================
 
-	Matrix2f estSigmaOne, estSigmaTwo;
-	Vector2f estMuOne, estMuTwo;
+	estMuOne = MLE::calculateSampleMean(sampleOne);
+	estMuTwo = MLE::calculateSampleMean(sampleTwo);
 
-	estMuOne = MLE::calculateSampleMean();
-	estMuTwo = MLE::calculateSampleMean();
-
-	estSigmaOne = MLE::calculateSampleCovariance();
-	estSigmaTwo = MLE::calculateSampleCovariance();
+	estSigmaOne = MLE::calculateSampleCovariance(sampleOne, estMuOne);
+	estSigmaTwo = MLE::calculateSampleCovariance(sampleTwo, estMuTwo);
 
 	sampleMis.clear();
 	misclassifiedOne = misclassifiedTwo = 0;
@@ -118,6 +119,10 @@ int main()
 	writeSamplesToFile("./results/Part1A-Estimated-Misclassified.txt", sampleMis, sampleMis);
 
 	generalOutput << "================================================\n Part 1A - (Esimated Parameters) \n================================================" << endl;
+	generalOutput << "Estimated Sample Mean: muOne=[" << estMuOne(0) << ", " << estMuOne(1) << "]" <<
+					 " muTwo=[" << estMuTwo(0) << ", " << estMuTwo(1) << "]" << endl;
+	generalOutput << "Estimated Sample Covaraince: \nsigmaOne" << endl;
+	generalOutput << estSigmaOne << endl << "sigmaTwo" << endl << estSigmaTwo << endl;
 	generalOutput << "Samples from one misclassified: " << misclassifiedOne << endl;
 	generalOutput << "Samples from two misclassified: " << misclassifiedTwo << endl;
 	generalOutput << "Total misclassified: " << misclassifiedOne + misclassifiedTwo << endl;
@@ -129,8 +134,20 @@ int main()
 	//================================================
 
 
+	
+
+
+
+
+
+
+
+
+
+
+
 	//================================================
-	// Begin Part 1B Configuration
+	// Begin Part 2A Configuration
 	//================================================
 
 	muOne << 1.0, 1.0;
@@ -141,7 +158,6 @@ int main()
 	sigmaTwo << 4.0, 0.0,
 				0.0, 8.0;
 
-	priorOne = priorTwo = 0.5;
 	misclassifiedOne = misclassifiedTwo = 0;
 
 	sampleOne = generator.generateSamples(muOne, sigmaOne);
@@ -150,11 +166,7 @@ int main()
 	sampleMis.clear();
 
 	//================================================
-	// End Part 1B Configuration
-	//================================================
-
-	//================================================
-	// Begin Part 1B Tests
+	// Begin Part 2A Tests (Known Covaraince and Mean)
 	//================================================
 
 	for(int i = 0; i < 10000; i++)
@@ -171,15 +183,64 @@ int main()
 		}
 	}
 
-	generalOutput << "================================================\n Part Two (A) - (Using the Bayesian Classifier) \n================================================" << endl;
+	writeSamplesToFile("./results/Part2A-Known-Misclassified.txt", sampleMis, sampleMis);
+
+	generalOutput << "================================================\n Part 2A - (Known Parameters) \n================================================" << endl;
 	generalOutput << "Samples from one misclassified: " << misclassifiedOne << endl;
 	generalOutput << "Samples from two misclassified: " << misclassifiedTwo << endl;
 	generalOutput << "Total misclassified: " << misclassifiedOne + misclassifiedTwo << endl;
 
-	writeSamplesToFile("./results/Part-TwoA-Misclassified.txt", sampleMis, sampleMis);
+	//================================================
+	// End Part 2A Tests (Known Covaraince and Mean)
+	//================================================
+
+	//================================================
+	// Begin Part 2A Tests (Esimated Covaraince and Mean)
+	//================================================
+
+	estMuOne = MLE::calculateSampleMean(sampleOne);
+	estMuTwo = MLE::calculateSampleMean(sampleTwo);
+
+	estSigmaOne = MLE::calculateSampleCovariance(sampleOne, estMuOne);
+	estSigmaTwo = MLE::calculateSampleCovariance(sampleTwo, estMuTwo);
+
+	sampleMis.clear();
+	misclassifiedOne = misclassifiedTwo = 0;
+
+	for(int i = 0; i < 10000; i++)
+	{
+		if(classifier.classifierCaseThree(sampleOne[i], estMuOne, estMuTwo, estSigmaOne, estSigmaTwo) == 2)
+		{
+			misclassifiedOne++;
+			sampleMis.push_back(sampleOne[i]);
+		}
+		if(classifier.classifierCaseThree(sampleTwo[i], estMuOne, estMuTwo, estSigmaOne, estSigmaTwo) == 1)
+		{
+			misclassifiedTwo++;
+			sampleMis.push_back(sampleTwo[i]);
+		}
+	}
+
+	writeSamplesToFile("./results/Part2A-Estimated-Misclassified.txt", sampleMis, sampleMis);
+
+	generalOutput << "================================================\n Part 2A - (Esimated Parameters) \n================================================" << endl;
+	generalOutput << "Estimated Sample Mean: muOne=[" << estMuOne(0) << ", " << estMuOne(1) << "]" <<
+					 " muTwo=[" << estMuTwo(0) << ", " << estMuTwo(1) << "]" << endl;
+	generalOutput << "Estimated Sample Covaraince: \nsigmaOne" << endl;
+	generalOutput << estSigmaOne << endl << "sigmaTwo" << endl << estSigmaTwo << endl;
+	generalOutput << "Samples from one misclassified: " << misclassifiedOne << endl;
+	generalOutput << "Samples from two misclassified: " << misclassifiedTwo << endl;
+	generalOutput << "Total misclassified: " << misclassifiedOne + misclassifiedTwo << endl;
+
+	writeSamplesToFile("./results/Part2A-Samples.txt", sampleOne, sampleTwo);
 
 	generalOutput.close();
 
+}
+
+int randIndex(int size)
+{
+	return (rand() / (float)RAND_MAX) * size;
 }
 
 void writeSamplesToFile(const char* fileName, vector<Vector2f> sampleOne, vector<Vector2f> sampleTwo)
