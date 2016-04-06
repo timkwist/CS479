dd8fa5da681d005a84a02d79fa41f7f725372689
Tim Kwist <Timkwist@hotmail.com>
2016-04-05 19:06:27 -0700

    Compute Eigen Faces encapsulated into a method
    Currently, matrix shows up with every other row being full of 0s.
    May need to consider going back to brute force read in, or simply
    doing some pre-processing to delete every row full of 0s

--- PA3-Eigenfaces/main.cpp
+++ PA3-Eigenfaces/main.cpp
@@ -3,23 +3,60 @@
 #include <dirent.h>
 #include <vector>
 #include <cstdlib>
-
+#include <ctime>
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <stdio.h>
 
 using namespace Eigen;
 using namespace std;
 
 #include "image.h"
 
+/* Helper Methods */
+namespace Eigen
+{
+    template<class Matrix>
+    void write_binary(const char* filename, const Matrix& matrix)
+    {
+        std::ofstream out(filename,ios::out | ios::binary | ios::trunc);
+        typename Matrix::Index rows=matrix.rows(), cols=matrix.cols();
+        out.write((char*) (&rows), sizeof(typename Matrix::Index));
+        out.write((char*) (&cols), sizeof(typename Matrix::Index));
+        out.write((char*) matrix.data(), rows*cols*sizeof(typename Matrix::Scalar) );
+        out.close();
+    }
+
+    template<class Matrix>
+    void read_binary(const char* filename, Matrix& matrix)
+    {
+        std::ifstream in(filename,ios::in | std::ios::binary);
+        typename Matrix::Index rows=0, cols=0;
+        in.read((char*) (&rows),sizeof(typename Matrix::Index));
+        in.read((char*) (&cols),sizeof(typename Matrix::Index));
+        matrix.resize(rows, cols);
+        in.read( (char *) matrix.data() , rows*cols*sizeof(typename Matrix::Scalar) );
+        in.close();
+    }
+}
+
+
+/* External methods */
 int readImageHeader(char[], int&, int&, int&, bool&);
 int readImage(char[], ImageType&);
+
+/* Internal methods */
 vector<VectorXi> readInTrainingFaces(const char *path, vector<VectorXi> &trainingFaces);
+void computeEigenFaces(vector<VectorXi> trainingFaces, VectorXf &averageFace, MatrixXf &eigenfaces, const char *path);
+
+
 
 int main()
 {
+    
     vector<VectorXi> trainingFaces;
-    MatrixXf A, eigenvectors;
+    MatrixXf eigenFaces;
     VectorXf averageFace;
     /**
      * TRAINING MODE
@@ -31,21 +68,10 @@ int main()
     //================================================
     // Compute Average Face and Eigenfaces
     //================================================
-    averageFace = VectorXf(trainingFaces[0].rows());
-    averageFace.fill(0);
-    for(auto it = trainingFaces.begin(); it != trainingFaces.end(); it++)
-    {
-        averageFace += (*it).cast<float>();
-    }
-    averageFace /= trainingFaces.size();
-    A = MatrixXf(averageFace.rows(), trainingFaces.size());
-    for(vector<VectorXi>::size_type i = 0; i < trainingFaces.size(); i++)
-    {
-        A.col(i) = trainingFaces[i].cast<float>() - averageFace;
-    }
-    eigenvectors = MatrixXf(trainingFaces.size(), trainingFaces.size());
-    eigenvectors = A.transpose()*A;
-    cout << eigenvectors.eigenvalues();
+    computeEigenFaces(trainingFaces, averageFace, eigenFaces, "fb_H");
+
+    
+
     //================================================
     // Interactive: Decide how many faces to keep
     //================================================
@@ -123,4 +149,41 @@ vector<VectorXi> readInTrainingFaces(const char *path, vector<VectorXi> &trainin
     }
 
     return trainingFaces;
+}
+
+void computeEigenFaces(vector<VectorXi> trainingFaces, VectorXf &averageFace, MatrixXf &eigenfaces, const char *path)
+{
+    char fileName[100];
+    EigenSolver<MatrixXf> solver;
+    MatrixXf A;
+    ofstream output;
+
+
+    averageFace = VectorXf(trainingFaces[0].rows());
+    averageFace.fill(0);
+    for(auto it = trainingFaces.begin(); it != trainingFaces.end(); it++)
+    {
+        averageFace += (*it).cast<float>();
+    }
+    averageFace /= trainingFaces.size();
+    A = MatrixXf(averageFace.rows(), trainingFaces.size());
+    for(vector<VectorXi>::size_type i = 0; i < trainingFaces.size(); i++)
+    {
+        A.col(i) = trainingFaces[i].cast<float>() - averageFace;
+    }
+    eigenfaces = MatrixXf(trainingFaces.size(), trainingFaces.size());
+    eigenfaces = A.transpose()*A;
+    solver.compute(eigenfaces, /* Compute eigenvectors = */ true);
+    
+    // Eigen saves eigenvectors in a strange format, so writing them to a file
+    // and then reading them back in saves them in a more "expected" format
+    sprintf(fileName, "%s-binary.dat", path);
+    Eigen::write_binary(fileName, solver.eigenvectors());
+    Eigen::read_binary(fileName,eigenfaces);
+
+    // Save eigenvectors to text file as well for viewing outside program
+    sprintf(fileName, "%s-EigenVectors.txt", path);
+    output.open(fileName);
+    output << eigenfaces;
+    output.close();
 }
