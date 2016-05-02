e614b069926c269f8c101623a1251bb49cdbcd9e
Shane <smelton@nevada.unr.edu>
2016-05-01 20:30:30 -0700

    Finished up PA4

--- PA4-Gender_Classification/main.cpp
+++ PA4-Gender_Classification/main.cpp
@@ -22,17 +22,19 @@ int main()
 
     // Read in Training face vectors
 
-    cout << "Reading in training data!" << endl;
+    // cout << "Reading in training data!" << endl;
 
     vector<VectorXf> maleTrainingFaces;
     vector<VectorXf> femaleTrainingFaces;
 
-    readInFaces(maleTrainingFaces, femaleTrainingFaces, "BayesData/16_20/new-trPCA_03.txt", "BayesData/16_20/TtrPCA_03.txt");
+    readInFaces(maleTrainingFaces, femaleTrainingFaces, "BayesData/48_60/new-trPCA_03.txt", "BayesData/48_60/TtrPCA_03.txt");
     //readInFaces(maleTrainingFaces, femaleTrainingFaces, "example.txt", "targets.txt");
     
+    // cout <<
+
     // Calculate covariance and mean
 
-    cout << "Calculating Means and Covariance Matricies" << endl;
+    // cout << "Calculating Means and Covariance Matricies" << endl;
 
     VectorXf maleMean       = MLE::calculateSampleMean(maleTrainingFaces);
     VectorXf femaleMean     = MLE::calculateSampleMean(femaleTrainingFaces);
@@ -40,45 +42,47 @@ int main()
     MatrixXf maleCovar      = MLE::calculateSampleCovariance(maleTrainingFaces, maleMean);
     MatrixXf femaleCovar    = MLE::calculateSampleCovariance(femaleTrainingFaces, femaleMean);
 
+    cout << maleCovar << endl;
+
     // Run Classifiers on test and validation data
 
-    cout << "Reading in test data!" << endl;
+    // cout << "Reading in test data!" << endl;
 
-    vector<VectorXf> maleTestFaces;
-    vector<VectorXf> femaleTestFaces;
+    // vector<VectorXf> maleTestFaces;
+    // vector<VectorXf> femaleTestFaces;
 
-    readInFaces(maleTestFaces, femaleTestFaces, "BayesData/16_20/testPCA_03.txt", "BayesData/16_20/TtestPCA_03.txt");
+    // readInFaces(maleTestFaces, femaleTestFaces, "BayesData/48_60/testPCA_03.txt", "BayesData/48_60/TtestPCA_03.txt");
 
-    cout << "Running classifier on test data!" << endl;
+    // cout << "Running classifier on test data!" << endl;
 
-    int correct = 0;
+    // int correct = 0;
 
-    for(unsigned int i = 0; i < maleTestFaces.size(); i++)
-    {
-        cout << i << endl;
-        if(BayesClassifier::classifierCaseThree(maleTestFaces[i], maleMean, femaleMean, maleCovar, femaleCovar) == 1)
-        {
-            correct++;
-        }
-    }
+    // for(unsigned int i = 0; i < maleTestFaces.size(); i++)
+    // {
+    //     cout << i << endl;
+    //     if(BayesClassifier::classifierCaseThree(maleTestFaces[i], maleMean, femaleMean, maleCovar, femaleCovar) == 1)
+    //     {
+    //         correct++;
+    //     }
+    // }
 
-    //Minus 8 here because for some reason femaleFaces seem to be corrupt or I might have copied the data wrong..
+    // //Minus 8 here because for some reason femaleFaces seem to be corrupt or I might have copied the data wrong..
 
-    for(unsigned int i = 0; i < femaleTestFaces.size() - 8; i++)
-    {
-        cout << i << endl;
-        if(BayesClassifier::classifierCaseThree(maleTestFaces[i], maleMean, femaleMean, maleCovar, femaleCovar) == 2)
-        {
-            correct++;
-        }
-    }
-    cout << endl << "RESULTS: " << endl;
+    // for(unsigned int i = 0; i < femaleTestFaces.size(); i++)
+    // {
+    //     cout << i << endl;
+    //     if(BayesClassifier::classifierCaseThree(femaleTestFaces[i], maleMean, femaleMean, maleCovar, femaleCovar) == 2)
+    //     {
+    //         correct++;
+    //     }
+    // }
+    // cout << endl << "RESULTS: " << endl;
 
-    cout << "\tTotal tests: \t" << (maleTestFaces.size() + femaleTestFaces.size() -8) << endl;
-    cout << "\tCorrect:     \t" << correct << endl;
-    cout << "\tPercentage:  \t" << ((float)correct / (float)(maleTestFaces.size() + femaleTestFaces.size() - 8)) << endl;
+    // cout << "\tTotal tests: \t" << (maleTestFaces.size() + femaleTestFaces.size()) << endl;
+    // cout << "\tCorrect:     \t" << correct << endl;
+    // cout << "\tPercentage:  \t" << ((float)correct / (float)(maleTestFaces.size() + femaleTestFaces.size())) << endl;
 
-    // Print results 
+    // // Print results 
 
 	return 0;
 }
